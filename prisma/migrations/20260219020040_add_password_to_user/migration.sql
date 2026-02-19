99ade7c99961438d984b0194c37257ede748cad5
GORACREW-YAMAGUCHI-084 <k-yamaguchi@goracrew.co.jp>
2026-02-19 14:30:02 +0900

    fix(migration): Use valid bcrypt hash for existing users to prevent lockout

--- prisma/migrations/20260219020040_add_password_to_user/migration.sql
+++ prisma/migrations/20260219020040_add_password_to_user/migration.sql
@@ -11,7 +11,7 @@ ALTER TABLE "User" ADD COLUMN "password" TEXT;
 -- Step 2: Backfill data (Set a default password or temporary value for existing users)
 -- Note: This 'temporary_password' should ideally be a hashed string if your app requires it,
 -- but for migration safety we just ensure it's not null.
-UPDATE "User" SET "password" = '$2b$10$EpOd/wQz/iGZ6u.6q0/uFO.Z6.Z6.Z6.Z6.Z6.Z6.Z6.Z6.Z6' WHERE "password" IS NULL;
+UPDATE "User" SET "password" = '$2b$10$Mg3eJbOy67Dk5u8KA2frQuc/6sAejle7AmU/CodkMfY/eQs5bHPVK' WHERE "password" IS NULL;
 
 -- Step 3: Add NOT NULL constraint
 ALTER TABLE "User" ALTER COLUMN "password" SET NOT NULL;
